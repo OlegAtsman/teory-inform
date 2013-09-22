0ad1722718b39ff57b0e88fba373181e97973c65
Admin <Admin@Admin-PC>
2013-09-22 13:22:12 +0300

    Refactor node Java

--- teory-inf/src/main/java/com/univer/teory/Node.java
+++ teory-inf/src/main/java/com/univer/teory/Node.java
@@ -5,19 +5,11 @@ public class Node implements Comparable<Node> {
 	
 	private char symbol;
 	private int freq;
-	private Node left;
-	private Node right;
 	
-	public Node(char symbol, int freq, Node left, Node right) {
+	public Node(char symbol, int freq) {
 		super();
 		this.symbol = symbol;
 		this.freq = freq;
-		this.left = left;
-		this.right = right;
-	}
-	
-	public boolean isLeaf() {
-		return (left == null && right == null);
 	}
 
 	public int compareTo(Node that) {
